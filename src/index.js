891ba2b829bd259f72e0953c142902020bf9aa31
Hafsa Egal <hafsaegal915@gmail.com>
2025-03-25 12:21:33 +0300

    fetch characters

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // Your code here
+//fetch characters from db.json
+
+function fetchCharacters() {
+  fetch("http://localhost:3000/characters")
+    .then((response) => response.json()) 
+    .then((characters) => {
+      const characterBar = document.getElementById("character-bar");
+      characterBar.innerHTML = "";})}; 
